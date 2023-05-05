dce487f5ad8291598aedea89e6554a01170a5c16
Wisthong Martinez <wisthongd@gmail.com>
2023-05-05 17:32:14 -0500

    Breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,8 @@ module.exports = {
       lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      xl: "1280px",
+      xl: "1800px",
       // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
