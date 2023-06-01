2a4ef05940639406d4b0dcbd6df1ee271429a099
Wisthong Martinez <wisthongd@gmail.com>
2023-06-01 15:20:45 -0500

    Cambios en dashboard master

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.40.51:4501/api',
-  // apiUrl: 'http://localhost:4501/api',
+  apiUrl: 'http://192.168.40.47:4501/api',
 };
