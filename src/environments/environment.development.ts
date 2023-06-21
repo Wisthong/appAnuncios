643161df42dc0eb48bdb634248a8cc07618f9d40
Wisthong Martinez <wisthongd@gmail.com>
2023-06-21 12:40:28 -0500

    Ajusten con el diseñador

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.17.177.141:4501/api',
-  // apiUrl: 'http://192.168.40.47:4501/api',
+  // apiUrl: 'http://172.17.177.141:4501/api',
+  apiUrl: 'http://192.168.40.47:4501/api',
 };
